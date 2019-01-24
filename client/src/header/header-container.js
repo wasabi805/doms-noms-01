4a5d66c22aa98cef7b177b488dd4890512cfd4c6
‘Tim <timothy.j.ocampo@gmail.com>
2019-01-23 16:08:46 -0800

    test if immutablejs logic will play well in Heroku

--- client/src/header/header-container.js
+++ client/src/header/header-container.js
@@ -3,12 +3,11 @@ import { connect } from 'react-redux';
 import Presenter      from './header-presenter';
 import * as Selectors from '../logic/selectors';
 
-
 const mapStateToProps = state => ({
 
 });
 
 const Container = connect(mapStateToProps)(Presenter);
 
-
+//
 export default Container;
