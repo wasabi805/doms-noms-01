7b743719d2b809a6a88d9dc5b8fb3ea216630d7a
‘Tim <timothy.j.ocampo@gmail.com>
2019-01-24 14:47:48 -0800

    installed bootstrap form module: completed styles to contact area.

--- client/src/contact/contact-presenter.js
+++ client/src/contact/contact-presenter.js
@@ -10,23 +10,44 @@ const Presenter = ({error, phone}) => {
       <div className='contact-overlay'>
 
       </div>
-      <form autoComplete="on" className="contact-form">
-        <div className="form-inputs">
-          <div className="left">
-            <FieldContainer field="name"    type="text"  autoComplete="name"  />
-            <FieldContainer field="email"   type="email" autoComplete="email" />
-            <FieldContainer field="phone"   type="tel"   autoComplete="phone" />
-            <FieldContainer field="subject" type="text"  autoComplete="off"   />
-          </div>
-          <div className="right">
-            <TextAreaContainer />
-          </div>
-        </div>
-        <div className="bottom">
-          <p className="error">{error}</p>
-          <button type="submit">Send</button>
-        </div>
-      </form>
+      {/*<form autoComplete="on" className="contact-form">*/}
+        {/*<div className="form-inputs">*/}
+          {/*<div className="left">*/}
+            {/*<FieldContainer field="name"    type="text"  autoComplete="name"  />*/}
+            {/*<FieldContainer field="email"   type="email" autoComplete="email" />*/}
+            {/*<FieldContainer field="phone"   type="tel"   autoComplete="phone" />*/}
+            {/*<FieldContainer field="subject" type="text"  autoComplete="off"   />*/}
+          {/*</div>*/}
+          {/*<div className="right">*/}
+            {/*<TextAreaContainer />*/}
+          {/*</div>*/}
+        {/*</div>*/}
+        {/*<div className="bottom">*/}
+          {/*<p className="error">{error}</p>*/}
+          {/*<button type="submit">Send</button>*/}
+        {/*</div>*/}
+      {/*</form>*/}
+
+      <h1 className='wood-bg-title'>Get in Touch</h1>
+        <p className='story-text'>Feel free to contact us about having us cater your next event!</p>
+
+        <form >
+            <div className="form-group">
+               <div className='column-left'>
+                   <input type="text" className="form-control" id="contact-name" placeholder="NAME:"/>
+                   <input type="email" className="form-control" id="contact-email" placeholder="EMAIL:"/>
+                   <input type="text" className="form-control" id="contact-phone" placeholder="PHONE:"/>
+                   <input type="text" className="form-control" id="contact-subject" placeholder="SUBJECT:"/>
+               </div>
+
+              <div className='column-right'>
+                  <textarea className="form-control" rows="3" placeholder="How can we help you today??"/>
+              </div>
+            </div>
+
+            <div className='button-container'><button type="submit" className="stamp is-approved">Submit</button></div>
+        </form>
+
 
       <h3 className="contact-text">To Speak with someone from our team,</h3>
 
