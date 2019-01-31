1e1982b22bf450adcefbb1d1a61e09b601cbef33
‘Tim <timothy.j.ocampo@gmail.com>
2019-01-30 16:28:37 -0800

    styled nav-modal buttons.

--- client/src/nav-modal/nav-modal-presenter.js
+++ client/src/nav-modal/nav-modal-presenter.js
@@ -8,40 +8,63 @@ import { configureAnchors } from 'react-scrollable-anchor'
 const Presenter = (props) => {
     configureAnchors({offset: -40, scrollDuration: 150})
     return (
-       <div className='navmodal-presenter'>
+       <React.Fragment>
            <input className="modal-state" id="modal-1" type="checkbox" checked={props.trueOrFalse} />
 
            <div className="modal">
 
                <label className="modal__bg" htmlFor="modal-1" onClick={props.handleNavModalClick}/>
                <div className="modal__inner">
-                   <label className="modal__close" htmlFor="modal-1"/>
+                   {/*<label className="modal__close" htmlFor="modal-1"/>*/}
 
                    <a href='#home' onClick={()=>props.handleNavModalClick()}>
-                       <button ><i className="fas fa-home"/>Home</button>
+                       <button >
+                           <i className="fas fa-home"/>
+                           <p>Home</p>
+                       </button>
                    </a>
 
                    <a href='#services' onClick={()=>props.handleNavModalClick()}>
-                       <button><i className="fas fa-utensils"/>Services</button>
+                       <button>
+                           <i className="fas fa-utensils"/>
+                           <p>Services</p>
+                       </button>
                    </a>
 
                    <a href='#food-menu' onClick={()=>props.handleNavModalClick()}>
-                       <button><i className="fas fa-book-open"/>Menu</button>
+                       <button>
+                           <i className="fas fa-book-open"/>
+                           <p>Menu</p>
+                       </button>
                    </a>
 
-                   <button><i className="fas fa-gift"/>Packages</button>
+
+                   <a href='#packages' onClick={()=>props.handleNavModalClick()}>
+                       <button>
+                           <i className="fas fa-gift"/>
+                           <p>Packages</p>
+                       </button>
+                   </a>
+
+
                    <a href='#gallery' onClick={()=>props.handleNavModalClick()}>
-                      <button> <i className="fas fa-images"/>Gallery</button>
+                      <button>
+                          <i className="fas fa-images"/>
+                          <p>Gallery</p>
+                      </button>
                    </a>
 
                    <a href='#contact' onClick={()=>props.handleNavModalClick()}>
-                       <button><i className="fas fa-phone-square"/>Contact</button>
+                       <button>
+                           <i className="fas fa-phone-square"/>
+                           <p>Contact</p>
+                       </button>
                    </a>
 
                </div>
            </div>
 
-       </div>
+       </React.Fragment>
     );
 };
 
